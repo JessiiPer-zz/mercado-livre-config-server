196f1800e6177e89995abc6d2045d4477f617621
JessiiPer <jessica_roch@hotmail.com>
2020-01-27 16:13:35 -0300

    Atualização do Path do arquivo de configuração

--- src/main/java/br/com/mercadolivre/config/ConfigServerApplication.java
+++ src/main/java/br/com/mercadolivre/config/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package br.com.mercadolivre.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
